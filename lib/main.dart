79c7bdd78a0b61676d72af5ea630decaa27d6872
ParkSohyunee <sohyunee016@gmail.com>
2024-05-07 15:41:44 +0900

    feat: Cards 위젯 구현

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class App extends StatelessWidget {
                 ],
               ),
               const SizedBox(
-                height: 120,
+                height: 80,
               ),
               Text(
                 'Total Balance',
@@ -78,7 +78,65 @@ class App extends StatelessWidget {
                       bgColor: Color(0xFF1F2123),
                       textColor: Colors.white)
                 ],
-              )
+              ),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const Text(
+                    'Wallets',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'View All',
+                    style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                    color: const Color(0xFF1F2123),
+                    borderRadius: BorderRadius.circular(25)),
+                child: const Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Euro',
+                              style: TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                          SizedBox(height: 10),
+                          Row(
+                            children: [
+                              Text('6 428',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white)),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text('EUR',
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white)),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
