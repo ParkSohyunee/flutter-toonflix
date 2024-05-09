e3c5daf80a7b6c5113e6ba375e547ff55f115463
ParkSohyunee <sohyunee016@gmail.com>
2024-05-09 18:33:16 +0900

    feat: 웹툰 클릭시 상세 스크린으로 이동하는 기능 구현 (Navigator, MaterialPageRoute)

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_toonflix/screens/detail_screen.dart';
 
 class Webtoon extends StatelessWidget {
   final String title, thumb, id;
@@ -7,34 +8,48 @@ class Webtoon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 230,
-          clipBehavior: Clip.hardEdge,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                    blurRadius: 15,
-                    offset: const Offset(8, 8),
-                    color: Colors.black.withOpacity(0.5)),
-              ]),
-          child: Image.network(
-            thumb,
-            headers: const {
-              'Referer': 'https://comic.naver.com',
-            },
+    return GestureDetector(
+      // 사용자가 스크린을 tap하는 것을 감지
+      onTap: () {
+        // Navigator.push를 사용해서 사용자가 다른 페이지에 온 것과 같은 효과
+        Navigator.push(
+            context,
+            // MaterialPageRoute - Stateless 위젯을 Route로 감싸서 다른 스크린으로 보이게 만드는 클래스
+            MaterialPageRoute(
+              builder: (context) =>
+                  DetailScreen(title: title, thumb: thumb, id: id),
+              fullscreenDialog: true,
+            ));
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 230,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(8, 8),
+                      color: Colors.black.withOpacity(0.5)),
+                ]),
+            child: Image.network(
+              thumb,
+              headers: const {
+                'Referer': 'https://comic.naver.com',
+              },
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          title,
-          style: const TextStyle(fontSize: 22),
-        )
-      ],
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            title,
+            style: const TextStyle(fontSize: 22),
+          )
+        ],
+      ),
     );
   }
 }
