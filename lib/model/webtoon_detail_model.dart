f245a21abd73c10a45fcc1ab2130e26e65768f81
ParkSohyunee <sohyunee016@gmail.com>
2024-05-12 11:57:53 +0900

    fix: WebtoonDetailModel 프로퍼티 수정

--- lib/model/webtoon_detail_model.dart
+++ lib/model/webtoon_detail_model.dart
@@ -1,10 +1,9 @@
 class WebtoonDetailModel {
-  final String title, about, genere, age, thumb;
+  final String title, about, genere, age;
 
   WebtoonDetailModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         about = json['about'],
         genere = json['genere'],
-        age = json['age'],
-        thumb = json['thumb'];
+        age = json['age'];
 }
